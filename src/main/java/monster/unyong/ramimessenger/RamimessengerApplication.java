b441648efab5add39028a07c43968e20a4cbd322
ImWonYong <ssassb123@daum.net>
2024-05-06 18:33:02 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/monster/unyong/ramimessenger/RamimessengerApplication.java
@@ -0,0 +1,13 @@
+package monster.unyong.ramimessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamimessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamimessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/monster/unyong/ramimessenger/RamimessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package monster.unyong.ramimessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamimessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
